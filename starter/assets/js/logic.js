59ec25fe1b8f7c5044d10d0b679b001da496f398
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2022-12-03 16:22:10 +0000

    uploading my questions

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,1 @@
+var currentQuestionIndex = 0;
---
+++ starter/assets/js/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        question1: 'What does HTML stand for?',
+        choices1: ['Hypertext markup language', 'Hyper markup language', 'Higher markup language', 'Hypertext makeup language'],
+        answer1: 'Hypertext markup language'
+
+    },
+    {
+        question2: 'What language is responsible for the structure of a webpage?',
+        choices2: ['Python', 'JavaScript', 'HTML', 'CSS'],
+        answer2: 'HTML'
+    },
+    {
+        question3: 'JavaScript allows hoisting of....?',
+        choices3: ['variables', 'Function definition', 'Function expression', 'None of the above'],
+        answer3: 'Function definition'
+    },
+    {
+        question4: 'Which tag inserts JavaScript into an HTML page?',
+        choices4: ['JS tag', 'JavaScript tag', 'Script Tag', 'Insert Tag'],
+        answer4: 'Script tag'
+    },
+    {
+        question5: 'The querySelector method returns the first element that matches a ____ selector?',
+        choices5: ['jQuery', 'CSS', 'React', 'JavaScript'],
+        answer5: 'CSS'
+    }
+
+    
+    
+    
+];
