de3b9ddcc0aa146196af9dafcfa000cdd44d7805
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2022-12-04 10:58:32 +0000

    loading my updated script

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,1 +1,66 @@
+
 var currentQuestionIndex = 0;
+var questionWrap = document.getElementById('question-wrapper');
+var btn = document.getElementById('start');
+var timer = document.getElementById('time');
+
+
+
+
+
+
+
+btn.addEventListener('click', function() {
+    questionWrap.classList.remove('hide');
+    timer.innerText = 70;
+    var runTimer = setInterval(function(){
+        timer.innerText--;  
+        if (timer.innerText <= 0) {
+            clearInterval(runTimer);
+        }
+    }, 1000);
+    
+
+    
+
+});
+
+    
+
+
+
+questionWrap.innerHTML = `
+<h3>${questions[currentQuestionIndex].main}</h3>
+<ul></ul>
+`;
+var ul = questionWrap.querySelector('ul');
+
+var arr = questions[currentQuestionIndex].choicesArr;
+
+for(var choice of arr){
+    ul.insertAdjacentHTML('beforeend', `<li>${choice}</li>`);
+
+}
+questionWrap.addEventListener('click', function(event) {
+    var answer = questions[currentQuestionIndex].answer;
+    var result = event.target.innerText;
+    var audioCorrect = new Audio('./assets/sfx/correct.wav');
+    var audioIncorrect = new Audio('./assets/sfx/incorrect.wav');
+    if(result !== answer){
+        timer.innerText -=10;
+        audioIncorrect.play();
+    } else {
+        audioCorrect.play();
+    }
+    currentQuestionIndex++;
+    
+    
+    
+
+});
+
+
+
+
+
+  
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,32 +1,29 @@
 var questions = [
     {
-        question1: 'What does HTML stand for?',
-        choices1: ['Hypertext markup language', 'Hyper markup language', 'Higher markup language', 'Hypertext makeup language'],
-        answer1: 'Hypertext markup language'
+        main: 'What does HTML stand for?',
+        choicesArr: ['Hypertext markup language', 'Hyper markup language', 'Higher markup language', 'Hypertext makeup language'],
+        answer: 'Hypertext markup language'
 
     },
     {
-        question2: 'What language is responsible for the structure of a webpage?',
-        choices2: ['Python', 'JavaScript', 'HTML', 'CSS'],
-        answer2: 'HTML'
+        main: 'What language is responsible for the structure of a webpage?',
+        choicesArr: ['Python', 'JavaScript', 'HTML', 'CSS'],
+        answer: 'HTML'
     },
     {
-        question3: 'JavaScript allows hoisting of....?',
-        choices3: ['variables', 'Function definition', 'Function expression', 'None of the above'],
-        answer3: 'Function definition'
+        main: 'JavaScript allows hoisting of....?',
+        choicesArr: ['variables', 'Function definition', 'Function expression', 'None of the above'],
+        answer: 'Function definition'
     },
     {
-        question4: 'Which tag inserts JavaScript into an HTML page?',
-        choices4: ['JS tag', 'JavaScript tag', 'Script Tag', 'Insert Tag'],
-        answer4: 'Script tag'
+        main: 'Which tag inserts JavaScript into an HTML page?',
+        choicesArr: ['JS tag', 'JavaScript tag', 'Script Tag', 'Insert Tag'],
+        answer: 'Script tag'
     },
     {
-        question5: 'The querySelector method returns the first element that matches a ____ selector?',
-        choices5: ['jQuery', 'CSS', 'React', 'JavaScript'],
-        answer5: 'CSS'
+        main: 'The querySelector method returns the first element that matches a ____ selector?',
+        choicesArr: ['jQuery', 'CSS', 'React', 'JavaScript'],
+        answer: 'CSS'
     }
 
-    
-    
-    
 ];
