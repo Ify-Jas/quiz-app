b4991ba3368af225cdb0f1564532a966f507a33e
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2022-12-04 15:17:28 +0000

    questions now showing

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,8 +1,9 @@
 
-var currentQuestionIndex = 0;
+//var currentQuestionIndex = 0;
 var questionWrap = document.getElementById('question-wrapper');
 var btn = document.getElementById('start');
 var timer = document.getElementById('time');
+var end = document.getElementById('end-screen');
 
 
 
@@ -25,39 +26,66 @@ btn.addEventListener('click', function() {
 
 });
 
+
+var currentQuestionIndex = 0;
+
     
+function quiz() {
+    
+    questionWrap.innerHTML = `
+    <h3>${questions[currentQuestionIndex].main}</h3>
+    <ul></ul>
+    `;
+    var ul = questionWrap.querySelector('ul');
 
+    var arr = questions[currentQuestionIndex].choicesArr;
 
+    for(var choice of arr){
+       ul.insertAdjacentHTML('beforeend', `<li>${choice}</li>`);
 
-questionWrap.innerHTML = `
-<h3>${questions[currentQuestionIndex].main}</h3>
-<ul></ul>
-`;
-var ul = questionWrap.querySelector('ul');
-
-var arr = questions[currentQuestionIndex].choicesArr;
-
-for(var choice of arr){
-    ul.insertAdjacentHTML('beforeend', `<li>${choice}</li>`);
-
-}
-questionWrap.addEventListener('click', function(event) {
-    var answer = questions[currentQuestionIndex].answer;
-    var result = event.target.innerText;
-    var audioCorrect = new Audio('./assets/sfx/correct.wav');
-    var audioIncorrect = new Audio('./assets/sfx/incorrect.wav');
-    if(result !== answer){
-        timer.innerText -=10;
-        audioIncorrect.play();
-    } else {
-        audioCorrect.play();
     }
-    currentQuestionIndex++;
+    questionWrap.addEventListener('click', function(event) {
+       var answer = questions[currentQuestionIndex].answer;
+       var result = event.target.innerText;
+       var audioCorrect = new Audio('./assets/sfx/correct.wav');
+       var audioIncorrect = new Audio('./assets/sfx/incorrect.wav');
+       if(result !== answer && currentQuestionIndex < 5) {
+           timer.innerText -=10;
+           audioIncorrect.play();
+           currentQuestionIndex++; 
+           console.log(currentQuestionIndex);
+           quiz();
+          
+        }else {
+           audioCorrect.play();
+           currentQuestionIndex++; 
+           console.log(currentQuestionIndex);
+           quiz();
+           
+        } 
+        if(currentQuestionIndex == 4 || timer.innerText == 0) {
+            end.classList.remove('hide');
+        }
     
-    
+            
+              
+    }); 
     
 
-});
+    
+    
+} 
+
+console.log(currentQuestionIndex);
+
+quiz();
+
+
+
+
+
+
+
 
 
 
